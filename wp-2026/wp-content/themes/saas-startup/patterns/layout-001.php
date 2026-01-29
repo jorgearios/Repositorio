d9fd99f184de58f42562cdc1fabc6f901eb4ca7e
Jorge Rios <2230492@upv.edu.mx>
2026-01-29 07:21:09 -0600

    Version 2 del proyecto

---
+++ wp-2026/wp-content/themes/saas-startup/patterns/layout-001.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Title: Layout 001
+ * Slug: saas-startup/layout-001
+ * Categories: layouts
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"},"blockGap":"var:preset|spacing|superbspacing-small","margin":{"top":"0","bottom":"0"}}},"className":"column-parent-border-radius","layout":{"type":"constrained","contentSize":"800px"}} -->
+<div class="wp-block-group alignfull column-parent-border-radius" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|superbspacing-xxsmall"}},"layout":{"type":"constrained","contentSize":""}} -->
+  <div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":3,"textColor":"primary","fontSize":"superbfont-small"} -->
+    <h3 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-superbfont-small-font-size"><?php esc_html_e("Contact Us", "saas-startup"); ?></h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:heading {"textAlign":"center","textColor":"contrast","fontSize":"superbfont-xlarge"} -->
+    <h2 class="wp-block-heading has-text-align-center has-contrast-color has-text-color has-superbfont-xlarge-font-size"><?php esc_html_e("We're Here to Support You", "saas-startup"); ?></h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center","textColor":"mono-2","fontSize":"superbfont-small"} -->
+    <p class="has-text-align-center has-mono-2-color has-text-color has-superbfont-small-font-size"><?php esc_html_e("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue lorem ut volutpat efficitur. Fusce justo magna, condimentum nec elementum sed.", "saas-startup"); ?></p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:columns -->
+  <div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"}}},"backgroundColor":"primary"} -->
+    <div class="wp-block-column has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:image {"width":"48px","height":"48px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/layout/icon-email.png"); ?>" alt="" style="object-fit:cover;width:48px;height:48px" /></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|mono-4"}}}},"textColor":"mono-4","fontSize":"superbfont-medium"} -->
+      <h3 class="wp-block-heading has-mono-4-color has-text-color has-link-color has-superbfont-medium-font-size" style="margin-top:0;margin-bottom:0"><?php esc_html_e("Email Support", "saas-startup"); ?></h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-4","fontSize":"superbfont-xsmall"} -->
+      <p class="has-mono-4-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("Email@example.com", "saas-startup"); ?></p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-4","fontSize":"superbfont-xsmall"} -->
+      <p class="has-mono-4-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("Email@example.com", "saas-startup"); ?></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"}}},"backgroundColor":"primary"} -->
+    <div class="wp-block-column has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:image {"width":"48px","height":"48px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/layout/icon-clock.png"); ?>" alt="" style="object-fit:cover;width:48px;height:48px" /></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|mono-4"}}}},"textColor":"mono-4","fontSize":"superbfont-medium"} -->
+      <h3 class="wp-block-heading has-mono-4-color has-text-color has-link-color has-superbfont-medium-font-size" style="margin-top:0;margin-bottom:0"><?php esc_html_e("Office Hours", "saas-startup"); ?></h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-4","fontSize":"superbfont-xsmall"} -->
+      <p class="has-mono-4-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("Monday - Friday", "saas-startup"); ?></p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-4","fontSize":"superbfont-xsmall"} -->
+      <p class="has-mono-4-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("08:30 - 16:00", "saas-startup"); ?></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+
+    <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|superbspacing-small","bottom":"var:preset|spacing|superbspacing-small","left":"var:preset|spacing|superbspacing-small","right":"var:preset|spacing|superbspacing-small"}}},"backgroundColor":"primary"} -->
+    <div class="wp-block-column has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--superbspacing-small);padding-right:var(--wp--preset--spacing--superbspacing-small);padding-bottom:var(--wp--preset--spacing--superbspacing-small);padding-left:var(--wp--preset--spacing--superbspacing-small)"><!-- wp:image {"width":"48px","height":"48px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+      <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/layout/icon-pin.png"); ?>" alt="" style="object-fit:cover;width:48px;height:48px" /></figure>
+      <!-- /wp:image -->
+
+      <!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|mono-4"}}}},"textColor":"mono-4","fontSize":"superbfont-medium"} -->
+      <h3 class="wp-block-heading has-mono-4-color has-text-color has-link-color has-superbfont-medium-font-size" style="margin-top:0;margin-bottom:0"><?php esc_html_e("Address", "saas-startup"); ?></h3>
+      <!-- /wp:heading -->
+
+      <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-4","fontSize":"superbfont-xsmall"} -->
+      <p class="has-mono-4-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("735 Plymouth Street", "saas-startup"); ?> </p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|superbspacing-xxsmall","bottom":"0"}}},"textColor":"mono-4","fontSize":"superbfont-xsmall"} -->
+      <p class="has-mono-4-color has-text-color has-superbfont-xsmall-font-size" style="margin-top:var(--wp--preset--spacing--superbspacing-xxsmall);margin-bottom:0"><?php esc_html_e("West Haven, CT 06516", "saas-startup"); ?></p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:column -->
+  </div>
+  <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
