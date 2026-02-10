a8384cf26b2bfea5022892430782b41ed27ba178
jorgearios <2230492@upv.edu.mx>
2026-02-10 17:46:58 -0600

    Ejemplos

--- ejemplo1.php
+++ ejemplo1.php
@@ -1,7 +1,8 @@
+<?php
 //primer ejemplo
 
 // Variable edad
-edad = 20;
+$edad = 20;
 
 // Condicional if
 if ($edad >= 18) {
@@ -9,39 +10,43 @@ if ($edad >= 18) {
 }
 
 // segundo ejemplo
-edad = 15;
+$edad = 15;
 
 if ($edad >= 18) {
     echo "Puede entrar";
-} else {
+}
+else {
     echo "No puede entrar";
 }
 
 //tercer ejemplo
 
-edad = 15;
+$edad = 15;
 
 if ($edad >= 18) {
     echo "Puede entrar";
-} else {
+}
+else {
     echo "No puede entrar";
 }
 
 //cuarto ejemplo
 
-calificacion = 8;
+$calificacion = 8;
 
 if ($calificacion >= 9) {
     echo "Excelente";
-} elseif ($calificacion >= 7) {
+}
+elseif ($calificacion >= 7) {
     echo "Aprobado";
-} else {
+}
+else {
     echo "Reprobado";
 }
 
 //quinto ejemplo
 
-dia = 3;
+$dia = 3;
 
 switch ($dia) {
     case 1:
@@ -60,31 +65,32 @@ switch ($dia) {
 //sexto ejemplo
 
 for ($i = 1; $i <= 5; $i++) {
-    echo "Número: ";
+    echo "Número: $i";
 }
 
 //septimo ejemplo
 
-i = 1;
+$i = 1;
 
 while ($i <= 5) {
-    echo "Número: "i";
+    echo "Número: $i";
     $i++;
 }
 
 //octavo ejemplo
 
-i = 1;
+$i = 1;
 
 do {
-    echo "Número: i";
+    echo "Número: $i";
     $i++;
 } while ($i <= 5);
 
 //noveno ejemplo
 
-colores = array("Rojo", "Verde", "Azul");
+$colores = array("Rojo", "Verde", "Azul");
 
 foreach ($colores as $color) {
-    echo $color . "";
+    echo $color . " ";
 }
+?>
